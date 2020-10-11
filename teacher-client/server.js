55cb181854acf884b4c7386e50038e78a78f120d
bulat faizrakhmanov <fayzrakhman7@gmail.com>
2020-10-11 14:26:26 +0300

    ads

--- teacher-client/server.js
+++ teacher-client/server.js
@@ -4,11 +4,14 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // send the user to index html page inspite of the url
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, req.originalUrl));
+	res.sendFile(path.resolve(__dirname, req.originalUrl));
 });
 
 app.listen(port);
