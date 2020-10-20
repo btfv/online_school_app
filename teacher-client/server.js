2541f6ea7621aa7bc589bddceefc0a05f2a8def4
bulat faizrakhmanov <fayzrakhman7@gmail.com>
2020-10-21 00:10:31 +0300

    trash

--- teacher-client/server.js
+++ teacher-client/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
+// the __dirname is the current directory from where the script is runninga
 app.use(express.static(__dirname + '/dist'));
 
 // send the user to index html page inspite of the url
